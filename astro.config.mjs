8ee53314dfe9a27c0ed33e33d950f1c3b733070f
kenosama <pc.tcanomorant@me.com>
2023-04-27 11:15:44 +0200

    Add RSSfeed to the site, Add Sitemap to the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astro-blog.netltify.app',
   integrations: [mdx(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  }), sitemap()]
 });
